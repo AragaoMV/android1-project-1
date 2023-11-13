e623b37c33250b43a891c32fd9efc53cef5ad114
AragaoMV <marcosvinisa@icloud.com>
2023-11-13 19:17:47 -0300

    Mudança de Tema

--- app/src/main/java/com/marcos/androidapp/HomeScreenActivity.java
+++ app/src/main/java/com/marcos/androidapp/HomeScreenActivity.java
@@ -2,13 +2,25 @@ package com.marcos.androidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HomeScreenActivity extends AppCompatActivity {
-
+    private TextView nomeLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        nomeLogin = findViewById(R.id.nomeLogin);
+        Intent intent = getIntent();
+        String nomeRecebido = intent.getStringExtra("KEY_NAME");
+        nomeLogin.setText(nomeRecebido);
+
+
+
+
+
     }
 }
--- app/src/main/java/com/marcos/androidapp/MainActivity.java
+++ app/src/main/java/com/marcos/androidapp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.marcos.androidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.SwitchCompat;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,20 +17,60 @@ public class MainActivity extends AppCompatActivity {
     private Button btnEntrar;
     private EditText nomeLogin;
 
+    //Tema
+    SwitchCompat switchMode;
+    boolean nightMode;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Tema
+        switchMode = findViewById(R.id.trocaTema);
+        sharedPreferences = getSharedPreferences("MODE", Context.MODE_PRIVATE);
+        nightMode = sharedPreferences.getBoolean("nightMode", false);
+
+        if (nightMode){
+            switchMode.setChecked(true);
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+        switchMode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(nightMode){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    editor = sharedPreferences.edit();
+                    editor.putBoolean("nightMode", false);
+                }else {
+
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                        editor = sharedPreferences.edit();
+                        editor.putBoolean("nightMode", true);
+                }
+                editor.apply();
+            }
+        });
+
+        //Tema
+
         nomeLogin = findViewById(R.id.nomeLogin);
         btnEntrar = findViewById(R.id.btnEntrar);
 
         btnEntrar.setOnClickListener(new View.OnClickListener() {
+
+
             @Override
             public void onClick(View view) {
+                String inputName = nomeLogin.getText().toString().trim();
                 Intent intent = new Intent(MainActivity.this, HomeScreenActivity.class);
+                intent.putExtra("KEY_NAME", inputName);
                 startActivity(intent);
+                finish();
             }
+
         });
 
     }
