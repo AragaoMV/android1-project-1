879abcd8a1ca4bbfaa7dfbfb76cc7ea3259f605c
AragaoMV <marcosvinisa@icloud.com>
2023-11-13 14:45:01 -0300

    Navegação

--- app/src/main/java/com/marcos/androidapp/MainActivity.java
+++ app/src/main/java/com/marcos/androidapp/MainActivity.java
@@ -1,14 +1,33 @@
 package com.marcos.androidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnEntrar;
+    private EditText nomeLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nomeLogin = findViewById(R.id.nomeLogin);
+        btnEntrar = findViewById(R.id.btnEntrar);
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, HomeScreenActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
